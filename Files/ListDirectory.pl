863010d11b5fa6bd2e40d1d211a53081db80dce6
david_allan <profissional@david-allan.com.br>
2017-01-11 14:50:51 -0200

    Added List directory script

---
+++ Files/ListDirectory.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Path::Class;
+
+my $dir = dir('\dev','Rust'); # \dev\text
+
+while (my $file = $dir->next) {
+    
+    # See if it is a directory and skip
+    next if $file->is_dir();
+    
+    # Print out the file name and path
+    print $file->stringify . "\n";
+}
